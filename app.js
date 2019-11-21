de9533ab84487f67952cbfb2acfad68309d33b0f
Mathias Onu <55839729+MathiasOnu@users.noreply.github.com>
2019-11-21 16:30:16 +0200

    fixed the bug.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var navSlide() {
+function navSlide() {
   var bigMac = document.querySelector(".burger");
   var noBurger = documen.querySelector(".menu-items");
-  bigMac.addEventListener('click', navSlide() {
+  bigMac.addEventListener('click', function() {
     noBurger.classList.toggle("nav-active");
   });
 };
 navSlide();
+
