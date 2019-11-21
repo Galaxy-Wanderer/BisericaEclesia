6e2a710086bd1df5b6cc5d644fc4cedf3d8644f3
Mathias Onu <55839729+MathiasOnu@users.noreply.github.com>
2019-11-21 16:16:42 +0200

    created a burger button.

---
+++ app.js
@@ -0,0 +1,8 @@
+var navSlide() {
+  var bigMac = document.querySelector(".burger");
+  var noBurger = documen.querySelector(".menu-items");
+  bigMac.addEventListener('click', navSlide() {
+    noBurger.classList.toggle("nav-active");
+  });
+};
+navSlide();
